73ab3559fef3489417e568305985639bf9c1d372
Daisuke Yoshida <nro2dai@gmail.com>
2021-08-18 11:33:55 +0900

    住吉区まち歩き向けにGISデータのリストの調整と、初期画面の位置情報の変更

--- CerdAR/CerdAR/osmViewController.swift
+++ CerdAR/CerdAR/osmViewController.swift
@@ -131,7 +131,7 @@ class osmViewController: UIViewController, CLLocationManagerDelegate, MGLMapView
         /* マップビューの設定 */
         mapView.frame = self.view.frame
         
-        mapView.camera = MGLMapCamera(lookingAtCenter: CLLocationCoordinate2D(latitude: 34.7, longitude: 135.5), altitude: 1200, pitch: 45, heading: 0)
+        mapView.camera = MGLMapCamera(lookingAtCenter: CLLocationCoordinate2D(latitude: 34.592337, longitude: 135.505284), altitude: 1200, pitch: 45, heading: 0)
         mapView.showsUserLocation = true // 現在地を表示する
         mapView.isPitchEnabled = true  // ジェスチャでの視点変更を許可しない
         mapView.showsScale = true
